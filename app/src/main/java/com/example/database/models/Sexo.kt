bc5d31ddb06b5d91e6a3934c386891c2b95998ad
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2023-06-07 11:13:42 -0500

    crear los modelos

---
+++ app/src/main/java/com/example/database/models/Sexo.kt
@@ -0,0 +1,3 @@
+package com.example.database.models
+
+data class Sexo(var id: Int, var name : String)
---
+++ app/src/main/java/com/example/database/models/Usuario.kt
@@ -0,0 +1,3 @@
+package com.example.database.models
+
+data class Usuario(var id:Int, var name:String, var email:String, var nacimiento : String, var sexoId : Int)
