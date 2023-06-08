a494e76ce14853039ec3de7acabdd82a5fbb58ee
JuanDiegoWS <mejiamaestrejuandiego@gmail.com>
2023-06-07 20:00:31 -0500

    llenar spinner

--- app/src/main/java/com/example/database/MainActivity.kt
+++ app/src/main/java/com/example/database/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.database
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.SpinnerAdapter
+import android.widget.Toast
+import com.example.database.DB.ManagerDB
+import com.example.database.databinding.ActivityMainBinding
+import com.example.database.models.Sexo
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ManagerDB.getInstance(this).TABLE_SEXO.create(Sexo(0, "Femenino"))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.spGenero.adapter = ArrayAdapter<Sexo>(this, android.R.layout.simple_spinner_dropdown_item, ManagerDB.getInstance().TABLE_SEXO.read().toList())
+        ManagerDB.getInstance().TABLE_SEXO.read().forEach {
+            Toast.makeText(this, it.name, Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/database/models/Sexo.kt
+++ app/src/main/java/com/example/database/models/Sexo.kt
@@ -1,3 +1,7 @@
 package com.example.database.models
 
-data class Sexo(var id: Int, var name : String)
+ class Sexo(var id: Int, var name : String){
+     override fun toString(): String {
+         return name
+     }
+ }
